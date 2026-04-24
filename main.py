edc07347068d739b7cdc43a4fa5a49dd2b3550a1
mbsurya19122004 <mbsurya19122004@gmail.com>
2026-04-24 16:48:11 +0530

    added support for compressed files

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import shutil
 import pathlib
 import time
 
+source = '/home/kashi/Downloads'
+pre_source='/home/kashi/Downloads'
+
+img= ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp', '.ico', '.heic', '.heif']
+vid=['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.mpeg', '.mpg', '.3gp', '.m4v', '.ts', '.vob']
+aud=['.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a', '.alac', '.aiff', '.amr', '.opus']
+prog=['.exe', '.bat', '.sh', '.py', '.js', '.jar', '.pl', '.rb', '.php', '.cpp', '.c', '.cs', '.java', '.go', '.swift', '.rs', '.out']
+doc= ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.epub', '.pages', '.md']
+compressed = ['.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz', '.tar.gz', '.tgz', '.tar.bz2', '.tbz2', '.tar.xz', '.txz', '.lz', '.lzma', '.z', '.cab', '.iso', '.dmg', '.apk']
+
+def init():
+    folders = ["imgs", "videos", "documents", "programs", "audio","compressed"]
+
+    for folder in folders:
+        path = f"{pre_source}/{folder}"
+        if not os.path.exists(path):
+            os.mkdir(path)
+
 def sort_files():
-    source = 'C:/Users/M B Suryanarayanan/VSCODE FOLDERS/PROJECTS/Auto File management script/UNSORTED'
-    pre_source='C:/Users/M B Suryanarayanan/VSCODE FOLDERS/PROJECTS/Auto File management script'
-
-    img= ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp', '.ico', '.heic', '.heif']
-    vid=['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.mpeg', '.mpg', '.3gp', '.m4v', '.ts', '.vob']
-    aud=['.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a', '.alac', '.aiff', '.amr', '.opus']
-    prog=['.exe', '.bat', '.sh', '.py', '.js', '.jar', '.pl', '.rb', '.php', '.cpp', '.c', '.cs', '.java', '.go', '.swift', '.rs', '.out']
-    doc= ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.epub', '.pages', '.md']
-
 
     allfiles = os.listdir(source)
 
@@ -30,9 +39,14 @@ def sort_files():
             shutil.move(f"{source}/{i}",f"{pre_source}/audio")
         elif file_extension in vid:
             shutil.move(f"{source}/{i}",f"{pre_source}/videos")
+        elif file_extension in compressed:
+            shutil.move(f"{source}/{i}",f"{pre_source}/compressed")
+        else:
+            pass
 
+init()
 while True:
     sort_files()
     time.sleep(3)
-        
+  
         
